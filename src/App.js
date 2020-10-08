213e45e94329ca886c4521229049bf6800d83bbb
dani198912 <bdaniel.mihalcea@gmail.com>
2020-10-08 22:34:37 +0300

    Started to implement a news section with Slack APi

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './App.scss';
 import Panel from './components/panel/panel.component';
 import FormPage from './components/form-page/formpage.component';
-import {ReactComponent as ArhiLogo} from './icons/logo_arhitextura.svg'
 
-import  Header from './components/header/header.component'
+
+import Header from './components/header/header.component'
 import FormularF8 from './formulare/f8-cerere-aut-constr/f8-cerere-autorizatie.formular'
 
 const App = () => {
   return (
-    <div className="main-container">
-      <Header className="dani"/>
-      <Panel side="left">
-        <ArhiLogo className="logo"/>
-      </Panel>
-
-      <FormPage formular = {<FormularF8 />}/>
-      <Panel side="right"/>
+    <div>
+      <Header />
+      <div className="main-container">
+        <Panel side="left">
+        </Panel>
+        <FormPage formular={<FormularF8 />} />
+        <Panel side="right" />
+      </div>
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import classes from './header.module.scss';
+import {ReactComponent as ArhiLogo} from '../../images/logo/Web_logo_Logo_patrat_negru.svg'
 
+function scrollUp(){
+    
+    window.scrollTo(0,0)
+}
 export default class Header extends Component {
     render() {
         return (
             <div className={classes.header_bar}>
+                <ArhiLogo className={classes.logo}
+                onClick={scrollUp}
+                />
+                {this.props.children}
             </div>
         )
     }
